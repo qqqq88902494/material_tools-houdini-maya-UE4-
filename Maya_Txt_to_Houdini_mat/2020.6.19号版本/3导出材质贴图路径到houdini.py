502168a12a5d8f8b549f7b85ddd03023647fe141
vfxmstc <62828014+qqqq88902494@users.noreply.github.com>
2020-06-19 14:37:55 +0800

    Add files via upload

---
+++ Maya_Txt_to_Houdini_mat/2020.6.19号版本/3导出材质贴图路径到houdini.py
@@ -0,0 +1,19 @@
+import maya.cmds as cmds
+import os
+cmds.select(hi=True)
+selObjList = cmds.ls(sl=True,type = 'lambert')
+material_name = []
+final_txt = ''
+txtpath = 'C:/temp/txt/'
+for node in selObjList:
+    try:
+        file_tex = cmds.listConnections(node + '.color')
+        file_tex_path = cmds.getAttr(file_tex[0] + '.fileTextureName')
+        final_txt = final_txt + str(node) + '-tex-' + str(file_tex_path)+'\n'
+    except:
+        pass
+if not os.path.exists(txtpath):
+    os.makedirs(txtpath)
+f = open(txtpath+'test.txt','w')
+f.write(final_txt)
+f.close()
