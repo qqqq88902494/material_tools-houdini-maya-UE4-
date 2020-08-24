d410f71e16b60e54a006834dbe463fe5b6991060
vfxmstc <62828014+qqqq88902494@users.noreply.github.com>
2020-08-24 14:29:07 +0800

    Delete 1删除命名空间.py

--- Maya_Txt_to_Houdini_mat/1删除命名空间.py
+++
@@ -1,8 +0,0 @@
-import maya.cmds as cmds
-
-for i in cmds.namespaceInfo(recurse=True,listOnlyNamespaces=True):
-    try:
-        if('UI' not in i and 'shared' not in i):
-            cmds.namespace(mergeNamespaceWithParent=True,removeNamespace=i)
-    except:
-        pass
